89a5b6467b375cf2118e580c0ff8ff9b9054c5a3
Thomas Gerleve <thomas.gerleve@zweitag.de>
2025-02-11 15:55:41 +0100

    Add JWT token authentication and set permissions

--- store_api/urls.py
+++ store_api/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from store_api import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stores/', views.StoreListCreateAPIView.as_view()),
     path('stores/<int:pk>', views.StoreRetrieveUpdateDestroyAPIView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- store_api/views.py
+++ store_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import generics
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
 from .models import Store
 from .serializers import StoreSerializer
 
@@ -11,7 +12,21 @@ class StoreListCreateAPIView(generics.ListCreateAPIView):
     filter_backends = [SearchFilter]
     search_fields = ['name', 'address', 'opening_hours']
 
+    def get_permissions(self):
+        self.permission_classes = [IsAuthenticated]
+        if self.request.method == 'POST':
+          if self.request.user.groups.filter(name='manager').exists():
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
 
 class StoreRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+          self.permission_classes = [IsAuthenticated]
+        else:
+          if self.request.user.groups.filter(name='manager').exists():
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
